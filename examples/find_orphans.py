4bddb89280d3cac7b0b52813fde6622deb044739
Drew Stinnet <drew@drewlink.com>
2008-10-13 00:00:57 +0000

    added a couple example scripts

---
+++ examples/find_orphans.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import os,sys
+
+## This is for the custom nagios module
+sys.path.insert(1, '../')
+from pynag.Parsers import config
+
+## Create the plugin option
+nc = config('/etc/nagios/nagios.cfg')
+nc.parse()
+
+## These are top level items.  We don't care if they dont' have a parent.
+## Things like datacenters should be displayed here
+top_level_items = ['main data center']
+
+
+
+orphan_hosts = []
+for host in nc['all_host']:
+	if not host.has_key('parents'):
+		if host['alias'] not in top_level_items:
+			orphan_hosts.append(host)
+
+if len(orphan_hosts) != 0:
+	print "The following hosts do not have parent items:"
+	for host in orphan_hosts:
+		print "%s (%s)" % (host['alias'], host['meta']['filename'])
+else:
+	print "No ophaned hosts found"
--- examples/parse_files.py
+++ examples/parse_files.py
@@ -2,11 +2,12 @@
 import os,sys
 
 ## This is for the custom nagios module
+sys.path.insert(1, '../')
 from pynag.Parsers import config
 
 ## Create the plugin option
 nc = config('/etc/nagios/nagios.cfg')
 nc.parse()
-nc._post_parse()
-for host in nc['all_host']:
+#nc._post_parse()
+for host in nc['all']:
 	nc.print_conf(host)
