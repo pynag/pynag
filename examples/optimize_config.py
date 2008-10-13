ec93c6ce57997e3355f46b763b9ad67b1e567dc1
Drew Stinnet <drew@drewlink.com>
2008-10-13 14:29:22 +0000

    Created script to optimize nagios configuration

---
+++ examples/optimize_config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import os,sys
+
+## This is for the custom nagios module
+#sys.path.insert(1, '../')
+from pynag.Parsers import config
+
+def is_ip(ip_address):
+	import socket
+	try:
+		mm = socket.inet_aton(ip_address)
+		return True # We got through that call without an error, so it is valid
+	except socket.error:
+		return False # There was an error, so it is invalid
+
+## Create the plugin option
+nc = config('/etc/nagios/nagios.cfg')
+nc.parse()
+
+nc.flag_all_commit()
+
+nc.commit()
