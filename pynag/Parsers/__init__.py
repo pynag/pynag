f6b176df57f2693069d25fddae5ee69316c44409
Pall Sigurdsson <pall.sigurdsson@gmail.com>
2011-05-25 16:49:33 +0000

    Metadata (author/maintainer) changed

--- pynag/Parsers/__init__.py
+++ pynag/Parsers/__init__.py
@@ -12,11 +12,11 @@ Python Nagios extensions
 
 __author__ = "Drew Stinnett"
 __copyright__ = "Copyright 2008, Drew Stinnett"
-__credits__ = ["Drew Stinnett"]
+__credits__ = ["Drew Stinnett", "Pall Sigurdsson"]
 __license__ = "GPL"
 __version__ = "0.3"
-__maintainer__ = "Drew Stinnett"
-__email__ = "drew@drewlink.com"
+__maintainer__ = "Pall Sigurdsson"
+__email__ = "palli@opensource.is"
 __status__ = "Development"
 
 def debug(text):
