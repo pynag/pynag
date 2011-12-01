eac17050d42ca10161b720d5d96841889069d2fc
Pall Sigurdsson <pall.sigurdsson@gmail.com>
2011-12-01 11:58:53 +0000

    new scripts added

--- scripts/check_nagios_needs_reload.py
+++ scripts/check_nagios_needs_reload.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 # Checks if nagios service needs a reload
 
-
+import sys
 try:
-	import sys
-	sys.path.append('/opt/pynag')
-	
 	from pynag.Parsers import config
 	
 	c = config(cfg_file='/etc/nagios/nagios.cfg')
@@ -13,11 +10,11 @@ try:
 	result = c.needs_reparse()
 	
 	if result == True:
-		print "Nagios configuration has changed since last restart"
+		print "Warning - Nagios configuration has changed since last restart"
 		sys.exit(1)
 	else:
-		print "Nagios service has been restarted since last config change"
+		print "OK - Nagios service has been restarted since last config change"
 		sys.exit(0)
 except Exception, e:
-	print "Error running script: %s" % e
+	print "Unknown - Error running script: %s" % e
 	sys.exit(3)
