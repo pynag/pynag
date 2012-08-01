48df79ddde380362bb80b033c9388b9c3fb27ff6
Tomas Edwardsson <tommi@tommi.org>
2012-08-01 10:06:31 +0000

    Removed static arguments to get_service

--- examples/Parsers/get_service_info.py
+++ examples/Parsers/get_service_info.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import sys
 
+if len(sys.argv) != 3:
+    sys.stderr.write("Usage:  %s 'Host Name' 'Service Description'\n" % (sys.argv[0]))
+    sys.exit(2)
+
+
 ## This is for the custom nagios module
 sys.path.insert(1, '../')
 from pynag.Parsers import config
@@ -10,6 +15,6 @@ from pynag.Parsers import config
 nc = config('/etc/nagios/nagios.cfg')
 nc.parse()
 
-service = nc.get_service("Users","wilmvpn")
+service = nc.get_service(sys.argv[1],sys.argv[2])
 
 print nc.print_conf(service)
