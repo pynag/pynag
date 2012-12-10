dfe9e554b75f40f6aaac2822bad22cf411846514
Pall Sigurdsson <palli@opensource.is>
2012-12-10 09:06:30 +0000

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ if __name__ == "__main__":
             'pynag.Model',
             'pynag.Model.EventHandlers',
             'pynag.Plugins',
-            'pynag.Plugins.new_threshold_syntax',
             'pynag.Parsers',
             'pynag.Control',
             'pynag.Utils',
