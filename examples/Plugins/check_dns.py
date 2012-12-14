7f10a10d924c326719b0333a0dccf65387ba370d
Pall Sigurdsson <palli@opensource.is>
2012-12-14 05:41:43 +0000

    Better documentation for PluginHelper examples

--- examples/Plugins/check_dns.py
+++ examples/Plugins/check_dns.py
@@ -1,25 +1,42 @@
 # check_dns.py -- Returns OK if a hostname resolves to any ip address
+
+# check_dns plugin will need some system libraries for DNS lookup
 from _socket import gaierror
 import socket
 import time
+
+# Import PluginHelper and some utility constants from the Plugins module
 from pynag.Plugins import PluginHelper,ok,warning,critical,unknown
 
+# Create an instance of PluginHelper()
 my_plugin = PluginHelper()
+
+# Our Plugin will need -H and -a attributes and we will use PluginHelpers wrapper around optparse for this:
 my_plugin.parser.add_option('-H', help="Hostname or ip address", dest="hostname")
 my_plugin.parser.add_option('-a', help="Expected Address", dest="address")
+
+# When parse_arguments is called some default options like --threshold and --no-longoutput are automatically added
 my_plugin.parse_arguments()
 
 
+#
+# Here starts Plugin-specific logic
+#
+
+# Get the hostname and expected address that was provided on the command-line
+# address will be optional, but we will throw and error if hostname is not provided
 hostname = my_plugin.options.hostname
 address = my_plugin.options.address
 if hostname is None:
     my_plugin.parser.error('-H argument is required')
 
+
 # Here comes the specific check logic
 try:
     start_time = time.time()
-    result = socket.gethostbyname( hostname )
+    result = socket.gethostbyname( hostname ) # result will contain the ip address resolved
     end_time = time.time()
+
     # If no address was specified with -a, then we return
     # OK if hostname resolved to anything at all
     if address is None or address == result:
@@ -33,10 +50,12 @@ try:
     run_time = end_time - start_time
     my_plugin.add_metric('run_time', run_time)
 except gaierror:
+    # If any exceptions happened in the code above, lets return a critical status
     my_plugin.status(critical)
     my_plugin.add_summary('Could not resolve host "%s"' % hostname )
 
-# Run check_all_metrics so end-user has the option of thresholding on run_time
+# when check_all_metrics() is run, any metrics we have added with add_metric() will be processed against
+# Thresholds (like --threshold). This part will allow our plugin users to alert on lookup_time
 my_plugin.check_all_metrics()
 
 # Print status output and exit
--- examples/Plugins/check_load.py
+++ examples/Plugins/check_load.py
@@ -1,18 +1,28 @@
 # check_load.py - Check load average. Thresholds can be specified from the commandline
 
+# Import PluginHelper and some utility constants from the Plugins module
 from pynag.Plugins import PluginHelper,ok,warning,critical,unknown
 
-
+# Create an instance of PluginHelper()
 helper = PluginHelper()
+
+# Optionally, let helper handle command-line arguments for us for example --threshold
+# Note: If your plugin needs any commandline arguments on its own (like --hostname) you should add them
+# before this step with helper.parser.add_option()
 helper.parse_arguments()
 
+
+# Here starts our plugin specific logic. Lets try to read /proc/loadavg
+# And if it fails, we exit immediately with UNKNOWN status
 try:
     content = open('/proc/loadavg').read()
 except Exception, e:
     helper.exit(summary="Could not read /proc/loadavg", long_output=str(e), exit_code=unknown, perfdata='')
 
 
+# We have read the contents of loadavg file. Lets put it in the summary of our plugin output:
 helper.add_summary("Load: %s" % content)
+
 
 # Read metrics from /proc/loadavg and add them as performance metrics
 load1,load5,load15,processes,last_proc_id = content.split()
@@ -27,7 +37,7 @@ helper.add_metric(label='load15',value=load15)
 helper.add_metric(label='running_processes',value=running)
 helper.add_metric(label='total_processes',value=total)
 
-# By default assume everything is ok:
+# By default assume everything is ok. Any thresholds specified with --threshold can overwrite this status:
 helper.status(ok)
 
 # Here all metrics will be checked against thresholds that are either
