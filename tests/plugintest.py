ef5785be4a67339ce8b77125cbfadbf80c8ad31c
Pall Valmundsson <pall.valmundsson@gmail.com>
2013-05-12 13:26:59 +0000

    function renaming

--- tests/plugintest.py
+++ tests/plugintest.py
@@ -27,19 +27,19 @@ class testPluginNoThreshold(unittest.TestCase):
     """
     All tests return OK since thresholds are not required
     """
-    def testNumber1(self):
+    def test_number_1(self):
         case = ''
         self.runExpect(case, 0, -23)
-    def testNumber2(self):
+    def test_number_2(self):
         case = ''
         self.runExpect(case, 0, 0)
-    def testNumber3(self):
+    def test_number_3(self):
         case = ''
         self.runExpect(case, 0, 2)
-    def testNumber4(self):
+    def test_number_4(self):
         case = ''
         self.runExpect(case, 0, 10)
-    def testNumber5(self):
+    def test_number_5(self):
         case = ''
         self.runExpect(case, 0, 15)
     
@@ -71,127 +71,127 @@ class testPluginHelper(unittest.TestCase):
     """
     Critical if "stuff" is over 20, else warn if over 10 (will be critical if "stuff" is less than 0)
     """
-    def testNumber1(self):
+    def test_number_1(self):
         case='--th=metric=fakedata,ok=0..10,warn=10..20'
         self.runExpect(case, -23, 2)
-    def testNumber2(self):
+    def test_number_2(self):
         case='--th=metric=fakedata,ok=0..10,warn=10..20'
         self.runExpect(case, 3, 0)
-    def testNumber3(self):
+    def test_number_3(self):
         case='--th=metric=fakedata,ok=0..10,warn=10..20'
         self.runExpect(case, 13, 1)
-    def testNumber4(self):
+    def test_number_4(self):
         case='--th=metric=fakedata,ok=0..10,warn=10..20'
         self.runExpect(case, 23, 2)
 
     """
     Same as above. Negative "stuff" is OK
     """
-    def testNumber5(self):
+    def test_number_5(self):
         case='--th=metric=fakedata,ok=inf..10,warn=10..20'
         self.runExpect(case, '-23', 0)
-    def testNumber6(self):
+    def test_number_6(self):
         case='--th=metric=fakedata,ok=inf..10,warn=10..20'
         self.runExpect(case, '3', 0)
-    def testNumber7(self):
+    def test_number_7(self):
         case='--th=metric=fakedata,ok=inf..10,warn=10..20'
         self.runExpect(case, '13', 1)
-    def testNumber8(self):
+    def test_number_8(self):
         case='--th=metric=fakedata,ok=inf..10,warn=10..20'
         self.runExpect(case, '23', 2)
 
     """
     Critical if "stuff" is over 20, else warn if "stuff" is below 10 (will be critical if "stuff" is less than 0)
     """
-    def testNumber9(self):
+    def test_number_9(self):
         case='--th=metric=fakedata,warn=0..10,crit=20..inf'
         self.runExpect(case, '-23', 0)
-    def testNumber10(self):
+    def test_number_10(self):
         case='--th=metric=fakedata,warn=0..10,crit=20..inf'
         self.runExpect(case, '3', 1)
-    def testNumber11(self):
+    def test_number_11(self):
         case='--th=metric=fakedata,warn=0..10,crit=20..inf'
         self.runExpect(case, '13', 0)
-    def testNumber12(self):
+    def test_number_12(self):
         case='--th=metric=fakedata,warn=0..10,crit=20..inf'
         self.runExpect(case, '23', 2)
 
     """
     Critical if "stuff" is less than 1
     """
-    def testNumber13(self):
+    def test_number_13(self):
         case='--th=metric=fakedata,ok=1..inf'
         self.runExpect(case, '-23', 2)
-    def testNumber14(self):
+    def test_number_14(self):
         case='--th=metric=fakedata,ok=1..inf'
         self.runExpect(case, '0', 2)
-    def testNumber15(self):
+    def test_number_15(self):
         case='--th=metric=fakedata,ok=1..inf'
         self.runExpect(case, '13', 0)
-    def testNumber16(self):
+    def test_number_16(self):
         case='--th=metric=fakedata,ok=1..inf'
         self.runExpect(case, '23', 0)
 
     """
     1-9 is warning, negative or above 10 is critical
     """
-    def testNumber17(self):
+    def test_number_17(self):
         case='--th=metric=fakedata,warn=1..9,crit=^0..10'
         self.runExpect(case, '-23', 2)
-    def testNumber18(self):
+    def test_number_18(self):
         case='--th=metric=fakedata,warn=1..9,crit=^0..10'
         self.runExpect(case, '0', 0)
-    def testNumber19(self):
+    def test_number_19(self):
         case='--th=metric=fakedata,warn=1..9,crit=^0..10'
         self.runExpect(case, '7', 1)
-    def testNumber20(self):
+    def test_number_20(self):
         case='--th=metric=fakedata,warn=1..9,crit=^0..10'
         self.runExpect(case, '23', 2)
 
     """
     The only noncritical range is 5:6
     """
-    def testNumber21(self):
+    def test_number_21(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '-23', 2)
-    def testNumber22(self):
+    def test_number_22(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '0', 2)
-    def testNumber23(self):
+    def test_number_23(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '2', 2)
-    def testNumber24(self):
+    def test_number_24(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '5', 0)
-    def testNumber25(self):
+    def test_number_25(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '6', 0)
-    def testNumber26(self):
+    def test_number_26(self):
         case='--th=metric=fakedata,ok=5..6'
         self.runExpect(case, '7', 2)
 
     """
     Critical if "stuff" is 10 to 20
     """
-    def testNumber27(self):
+    def test_number_27(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '-23', 0)
-    def testNumber28(self):
+    def test_number_28(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '0', 0)
-    def testNumber29(self):
+    def test_number_29(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '2', 0)
-    def testNumber30(self):
+    def test_number_30(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '10', 2)
-    def testNumber31(self):
+    def test_number_31(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '15', 2)
-    def testNumber32(self):
+    def test_number_32(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '20', 2)
-    def testNumber33(self):
+    def test_number_33(self):
         case='--th=metric=fakedata,ok=^10..20'
         self.runExpect(case, '23', 0)
     
@@ -236,123 +236,123 @@ class testPlugin(unittest.TestCase):
     """
     Critical if "stuff" is over 20, else warn if over 10 (will be critical if "stuff" is less than 0)
     """
-    def testNumber1(self):
+    def test_number_1(self):
         case = '-w 10 -c 20'
         self.runExpect(case, 2, -23)
-    def testNumber2(self):
+    def test_number_2(self):
         case = '-w 10 -c 20'
         self.runExpect(case, 0, 3)
-    def testNumber3(self):
+    def test_number_3(self):
         case = '-w 10 -c 20'
         self.runExpect(case, 1, 13)
-    def testNumber4(self):
+    def test_number_4(self):
         case = '-w 10 -c 20'
         self.runExpect(case, 2, 23)
 
     """
     Same as above. Negative "stuff" is OK
     """    
-    def testNumber5(self):
+    def test_number_5(self):
         case = '-w ~:10 -c ~:20'
         self.runExpect(case, 0, -23)
-    def testNumber6(self):
+    def test_number_6(self):
         case = '-w ~:10 -c ~:20'
         self.runExpect(case, 0, 3)
-    def testNumber7(self):
+    def test_number_7(self):
         case = '-w ~:10 -c ~:20'
         self.runExpect(case, 1, 13)
-    def testNumber8(self):
+    def test_number_8(self):
         case = '-w ~:10 -c ~:20'
         self.runExpect(case, 2, 23)
 
     """
     Critical if "stuff" is over 20, else warn if "stuff" is below 10 (will be critical if "stuff" is less than 0)
     """
-    def testNumber9(self):
+    def test_number_9(self):
         case = '-w 10: -c 20'
         self.runExpect(case, 2, -23)
-    def testNumber10(self):
+    def test_number_10(self):
         case = '-w 10: -c 20'
         self.runExpect(case, 1, 3)
-    def testNumber11(self):
+    def test_number_11(self):
         case = '-w 10: -c 20'
         self.runExpect(case, 0, 13)
-    def testNumber12(self):
+    def test_number_12(self):
         case = '-w 10: -c 20'
         self.runExpect(case, 2, 23)
 
     """
     Critical if "stuff" is less than 1
     """
-    def testNumber13(self):
+    def test_number_13(self):
         case = '-c 1:'
         self.runExpect(case, 2, -23)
-    def testNumber14(self):
+    def test_number_14(self):
         case = '-c 1:'
         self.runExpect(case, 2, 0)
-    def testNumber15(self):
+    def test_number_15(self):
         case = '-c 1:'
         self.runExpect(case, 0, 13)
-    def testNumber16(self):
+    def test_number_16(self):
         case = '-c 1:'
         self.runExpect(case, 0, 23)
 
     """
     1-9 is warning, negative or above 10 is critical
     """
-    def testNumber17(self):
+    def test_number_17(self):
         case = '-w ~:0 -c 10'
         self.runExpect(case, 2, -23)
-    def testNumber18(self):
+    def test_number_18(self):
         case = '-w ~:0 -c 10'
         self.runExpect(case, 0, 0)
-    def testNumber19(self):
+    def test_number_19(self):
         case = '-w ~:0 -c 10'
         self.runExpect(case, 1, 7)
-    def testNumber20(self):
+    def test_number_20(self):
         case = '-w ~:0 -c 10'
         self.runExpect(case, 2, 23)
 
     """
     The only noncritical range is 5:6
     """
-    def testNumber21(self):
+    def test_number_21(self):
         case = '-c 5:6'
         self.runExpect(case, 2, -23)
-    def testNumber22(self):
+    def test_number_22(self):
         case = '-c 5:6'
         self.runExpect(case, 2, 0)
-    def testNumber23(self):
+    def test_number_23(self):
         case = '-c 5:6'
         self.runExpect(case, 2, 2)
-    def testNumber24(self):
+    def test_number_24(self):
         case = '-c 5:6'
         self.runExpect(case, 0, 5)
-    def testNumber25(self):
+    def test_number_25(self):
         case = '-c 5:6'
         self.runExpect(case, 0, 6)
 
     """
     Critical if "stuff" is 10 to 20
     """
-    def testNumber26(self):
+    def test_number_26(self):
         case = '-c @10:20'
         self.runExpect(case, 0, -23)
-    def testNumber27(self):
+    def test_number_27(self):
         case = '-c @10:20'
         self.runExpect(case, 0, 0)
-    def testNumber28(self):
+    def test_number_28(self):
         case = '-c @10:20'
         self.runExpect(case, 0, 2)
-    def testNumber29(self):
+    def test_number_29(self):
         case = '-c @10:20'
         self.runExpect(case, 2, 10)
-    def testNumber30(self):
+    def test_number_30(self):
         case = '-c @10:20'
         self.runExpect(case, 2, 15)
-    def testNumber31(self):
+    def test_number_31(self):
         case = '-c @10:20'
         self.runExpect(case, 2, 20)
-    def testNumber32(self):
+    def test_number_32(self):
         case = '-c @10:20'
         self.runExpect(case, 0, 23)
