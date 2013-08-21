0c71658b91a34dfb51df9bae2cc5e51206b0f439
Daniel Kimsey <dekimsey@ufl.edu>
2013-08-21 14:52:31 -0400

    Fix TypeError in PynagError's __init__ due to Python 2.4
    Python 2.4 Exception's are not new-style classes. Therefore, we cannot use
    the super(Parent, self) syntax.
    
    This fixes errors in test_invalid_threshold and test_invalid_range
    
          File "/home/nagios/pynag/pynag/Utils/__init__.py", line 48, in __init__
            super(self.__class__, self).__init__(message, *args,**kwargs)
        TypeError: super() argument 1 must be type, not classobj

--- pynag/Utils/__init__.py
+++ pynag/Utils/__init__.py
@@ -45,7 +45,10 @@ class PynagError(Exception):
         self.errorcode = errorcode
         self.message = message
         self.errorstring = errorstring
-        super(self.__class__, self).__init__(message, *args,**kwargs)
+        try:
+            super(self.__class__, self).__init__(message, *args,**kwargs)
+        except TypeError:  # Python 2.4 is fail
+            Exception.__init__(self, message, *args,**kwargs)
 
 
 def runCommand(command, raise_error_on_fail=False):
