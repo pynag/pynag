549a8c5c38c7118f2e8692e7051bb2a01a8d17cb
Tomas Edwardsson <tommi@tommi.org>
2014-06-13 14:29:59 +0000

    Simplify and make available pynag.Utils.CheckResult
    Basic initialization
    
    from pynag.Utils import CheckResult
    
    check_result = CheckResult('/tmp')
    
    check_result.service_result(
        host_name="testhost",
        service_description="test service",
        output="OK This is nice!",
    )
    
    check_result.service_result(
        host_name="testhost",
        service_description="bad service",
        output="Oh darn!",
        return_code=2,
    )
    
    check_result.submit()

--- tests/test_nagios_checkresult.py
+++ tests/test_nagios_checkresult.py
@@ -1,60 +1,72 @@
 #! /usr/bin/python
 
+import os
+import sys
+
+# Make sure we import from working tree
+pynagbase = os.path.dirname(os.path.realpath(__file__ + "/.."))
+sys.path.insert(0, pynagbase)
+
 import unittest2 as unittest
-import time
-import textwrap
-from pynag.Utils import nagios_checkresult
+from tempfile import mkdtemp
+from pynag.Utils import CheckResult
 
 class TestNagiosCheckResult(unittest.TestCase):
+    def _findstr(self, file_name, string):
+        """Returns true if string found in file"""
+        fh = open(file_name)
+        output = fh.read()
+        fh.close()
+        return string in output
+
     def setUp (self):
-	self.maxDiff = None
-	#this is how checkresult file should look like
-	self.checkresult = textwrap.dedent("""\
-	### Active Check Result File ###
-	file_time=1400347643.73
+        self.tempdir = mkdtemp()
+        self.check_result = CheckResult(self.tempdir)
 
-	### Nagios Host Check Result ###
-	# Time: Sat May 17 22:57:23 2014
-	host_name=xyz
-	check_type=0
-	check_options=0
-	scheduled_check=1
-	reschedule_check=1
-	latency=0.1
-	start_time=1399732963.0
-	finish_time=1399732963.0
-	early_timeout=0
-	exited_ok=1
-	return_code=0
-	output= Host (xyz) UP\\n
+    def test_header(self):
+        """Test if the result header is correct"""
+        filename = self.check_result.submit()
 
-	### Nagios Service Check Result ###
-	# Time: Sat May 17 22:57:23 2014
-	host_name=xyz
-	service_description=Total processes
-	check_type=0
-	check_options=0
-	scheduled_check=1
-	reschedule_check=1
-	latency=0.1
-	start_time=1399732963.0
-	finish_time=1399732963.0
-	early_timeout=0
-	exited_ok=1
-	return_code=0
-	output=Total processes OK- Total processes 288 \\n\n""")
-	
-    def test_checkresult(self):
-	#generate checkresult file by sending data to GenerateNagiosCheckResult 
-	ng = nagios_checkresult.GenerateNagiosCheckResult()	
-	ng.create('/var/lib/nagios3/spool/checkresults', 1400347643.73)
-	ng.build_host('Sat May 17 22:57:23 2014', 'xyz', 0, 0, 1, 1, 0.1, str(1399732963.0), str(1399732963.0), 0, 1, 0, "")
-	ng.build_service('Sat May 17 22:57:23 2014', 'xyz', 'Total processes', 0, 0, 1, 1, 0.1, str(1399732963.0), str(1399732963.0), 0, 1, 0, 288, "", "")
-	#fname is the name of checkresult file generated
-	fname = ng.submit()
-	self.testfile = open(fname).read()
-	#compare the expected checkresult file with generated checkresult file
-	self.assertMultiLineEqual(self.testfile, self.checkresult, msg=None)
+        self.assertTrue(self._findstr(
+            filename,
+            "### Active Check Result File ###\n"
+        ))
+
+        self.assertTrue(self._findstr(
+            filename,
+            "file_time=%s\n" % self.check_result.file_time
+        ))
+
+    def test_service(self):
+        """Test if the service is created correctly"""
+        self.check_result.service_result('testhost', 'test service')
+        filename = self.check_result.submit()
+
+        self.assertTrue(self._findstr(
+            filename,
+            '\nhost_name=testhost\n'
+        ))
+        self.assertTrue(self._findstr(
+            filename,
+            '\nservice_description=test service\n'
+        ))
+
+    def test_host(self):
+        """Test if the host is created correctly"""
+        self.check_result.host_result('testhost')
+        filename = self.check_result.submit()
+
+        self.assertTrue(self._findstr(
+            filename,
+            '\nhost_name=testhost\n'
+        ))
+
+def __findstr(self, file, string):
+    """Returns true if string found in file"""
+    fh = open(file)
+    output = fh.read()
+    fh.close()
+    return string in output
 
 if __name__ == '__main__':
     unittest.main()
