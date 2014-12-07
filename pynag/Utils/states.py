4cee142543e28dbbae97850993c6f0ece18ce9f0
Pall Sigurdsson <palli@opensource.is>
2014-12-07 17:33:36 +0100

    New module: Utils.state
    Its purpose is to be the one-and-for-all representation of nagios states.
    
    This should eventually obsolete the constants and dictionaries in pynag.Plugins

---
+++ pynag/Utils/states.py
@@ -0,0 +1,160 @@
+"""Constants and convenience functions for host and service states."""
+import pynag.errors
+
+OK, WARNING, CRITICAL, UNKNOWN = 0, 1, 2, 3
+UP, DOWN, UNREACHABLE = 0, 1, 2
+
+_SERVICE_TEXT_TO_INT = {
+    'ok': OK,
+    'warning': WARNING,
+    'critical': CRITICAL,
+    'unknown': UNKNOWN,
+
+    # Common abbreviations:
+    'warn': WARNING,
+    'crit': CRITICAL,
+    'w': WARNING,
+    'c': CRITICAL,
+    'u': UNKNOWN,
+
+    '0': OK,
+    '1': WARNING,
+    '2': CRITICAL,
+    '3': UNKNOWN,
+}
+
+_HOST_TEXT_TO_INT = {
+    'UP': UP,
+    'DOWN': DOWN,
+    'UNREACHABLE': UNREACHABLE,
+
+    '0': UP,
+    '1': DOWN,
+    '2': UNREACHABLE
+}
+
+_SERVICE_INT_TO_TEXT = {
+    OK: 'OK',
+    WARNING: 'Warning',
+    CRITICAL: "Critical",
+    UNKNOWN: 'Unknown'
+}
+
+_HOST_INT_TO_TEXT = {
+    UP: 'Up',
+    DOWN: 'Down',
+    UNREACHABLE: 'Unreachable'
+}
+
+
+class UnknownState(pynag.errors.PynagError):
+    """Raised when a state was inputed that we do not recognize."""
+
+
+def service_state_to_int(state):
+    """Converts from strings like OK to states like 0.
+
+    Args:
+        State: string. Can be in the form of 'ok', 'warning', 'critical', '1', etc.
+
+    Returns:
+        Integer. A nagios corresponding to the free text state inputted.
+
+    Raises:
+        UnknownState: If no match can be found for 'state'.
+
+    Examples:
+        >>> service_state_to_int('ok')
+        0
+        >>> service_state_to_int('Warning')
+        1
+        >>> service_state_to_int('critical')
+        2
+        >>> service_state_to_int('UNKNOWN')
+        3
+        >>> service_state_to_int('0')
+        0
+        >>> service_state_to_int('1')
+        1
+        >>> service_state_to_int('2')
+        2
+        >>> service_state_to_int('3')
+        3
+    """
+    text = str(state).lower()
+    try:
+        return _SERVICE_TEXT_TO_INT[text]
+    except KeyError:
+        raise UnknownState('Do not know how to handle state %s' % state)
+
+
+def service_state_to_string(state):
+    """Converts from integer or text to a formal text representation of a nagios state.
+
+    Args:
+        State: integer or string. e.g. 0,1,2,3 or 'warn', 'crit'
+
+    Returns:
+        String. Formal description of a specified nagios state.
+
+    Raises:
+        UnknownState: If no match can be found for 'state'.
+
+    Examples:
+        >>> service_state_to_string(0)
+        'OK'
+        >>> service_state_to_string(1)
+        'Warning'
+        >>> service_state_to_string(2)
+        'Critical'
+        >>> service_state_to_string(3)
+        'Unknown'
+        >>> service_state_to_string('ok')
+        'OK'
+        >>> service_state_to_string('warn')
+        'Warning'
+    """
+    integer = service_state_to_int(state)
+    try:
+        return _SERVICE_INT_TO_TEXT[integer]
+    except KeyError:
+        raise UnknownState('Do not know how to handle state %s' % state)
+
+
+def host_state_to_int(state):
+    """Converts from strings like UP to states like 0.
+
+    Args:
+        State: string. Can be in the form of 'UP', 'Down', 'Unreachable', '1', etc.
+
+    Returns:
+        String. Formal description of a specified nagios state.
+
+    Raises:
+        UnknownState: If no match can be found for 'state'.
+
+    """
+    text = str(state).lower()
+    try:
+        return _HOST_TEXT_TO_INT[text]
+    except KeyError:
+        raise UnknownState('Do not know how to handle state %s' % state)
+
+
+def host_to_string(state):
+    """Converts from integer or text to a formal text representation of a nagios state.
+
+    Args:
+        State: string. Can be in the form of 'ok', 'warning', 'critical', '1', etc.
+
+    Returns:
+        Integer. A nagios corresponding to the free text state inputted.
+
+    Raises:
+        UnknownState: If no match can be found for 'state'.
+    """
+    integer = host_state_to_int(state)
+    try:
+        return _SERVICE_INT_TO_TEXT[integer]
+    except KeyError:
+        raise UnknownState('Do not know how to handle state %s' % state)
